2e64bc6197415a345bfd96f56fa151042f5a64d2
Isaac Lozano <Isaac-Lozano@users.noreply.github.com>
2019-01-15 22:29:06 -0800

    build.rs: add changes to prevent constant protobuf recompiling
    Added a loop to print out "cargo:rerun-if-changed=FILE" for every input
    file so that code isn't regenerated on every build and only when the
    protbuf files are changed.

--- backuplib/build.rs
+++ backuplib/build.rs
@@ -1,9 +1,14 @@
 extern crate protoc_rust_grpc;
 
 fn main() {
+    let input_files = ["protos/baacup.proto"];
+    for filename in input_files.iter() {
+        println!("cargo:rerun-if-changed={}", filename);
+    }
+
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
         out_dir: "src/proto",
-        input: &["protos/baacup.proto"],
+        input: &input_files,
         includes: &["protos"],
         rust_protobuf: true, // also generate protobuf messages, not just services
         ..Default::default()
